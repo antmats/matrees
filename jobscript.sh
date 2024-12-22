436cda654909cabd8a6fd9b5ffed758b10ec94ee
antmats <anton.mattsson94@gmail.com>
2024-12-22 22:56:18 +0100

    switch back to tetralith

--- jobscript.sh
+++ jobscript.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-#SBATCH -A NAISS2024-5-480
-#SBATCH -p alvis
+#SBATCH -A NAISS2024-22-285
+#SBATCH -p tetralith
 #SBATCH -N 1
-#SBATCH --gpus-per-node=A40:1
+#SBATCH --exclusive
 #SBATCH -t 1-0:0  # days-hours:minutes
-#SBATCH --output=/mimer/NOBACKUP/groups/inpole/matrees/logs/%x_%A_%a.out
+#SBATCH --output=/proj/healthyai/matrees/logs/%x_%A_%a.out
 
-container_path=/mimer/NOBACKUP/groups/inpole/matrees/matrees_env.sif
+container_path=/proj/healthyai/matrees/matrees_env.sif
 
 input_file=$1
 eval `head -n $SLURM_ARRAY_TASK_ID $input_file | tail -1`
@@ -19,7 +19,7 @@ if [ -z "$n_features" ]; then
     n_features=20
 fi
 
-apptainer exec --bind "${TMPDIR}:/mnt" $container_path python main.py \
+apptainer exec --bind "/proj:/proj,${TMPDIR}:/mnt" $container_path python main.py \
     --estimator_alias "$estimator_alias" \
     --n_train $n_train \
     --n_test $n_test \
